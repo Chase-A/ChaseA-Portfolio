6e10b1258a0b39948b1ab4f0850b7f5c555209f9
Chase Almy <chase@chases-air.mynetworksettings.com>
2023-06-24 13:51:11 -0400

    underlines

---
+++ src/components/Nav.svelte
@@ -0,0 +1,23 @@
+<script>
+
+import { page } from '$app/stores';
+
+let links = ['about', 'experience', 'projects']
+
+function toCapitalCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\w/g, function(match) {
+    return match.toUpperCase();
+  });
+}
+
+</script>
+
+
+
+
+
+<nav class="flex sm:flex-col justify-between sm:justify-self-end">
+    {#each links as link}
+        <a class="text-right hover:underline {('/'+ link) === $page.url.pathname ? 'underline': ''}" href="/{link}">{toCapitalCase(link)}</a>
+    {/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../app.css";
+    import Nav from "../components/Nav.svelte";
 </script>
 <div class="flex justify-center items-center h-screen">
     <div class="grid sm:grid-cols-[auto,1fr] gap-4 m-auto w-max">
@@ -8,13 +9,7 @@
                 <h1 class="text-3xl font-bold">Chase Almy</h1>
                 <h4>Process Focused UI Developer</h4>
             </div>
-        
-        <nav class="flex sm:flex-col justify-between sm:justify-self-end">
-            <a href="/about" class=" text-right">About</a>
-            <a href="/experience" class=" text-right">Experience</a>
-            <a href="/projects" class=" text-right">Projects</a>
-        </nav>
-        
+            <Nav/>
         <slot />
     </div>
 
