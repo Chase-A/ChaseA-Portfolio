862425918f39ce90afb31ec1eb8eeb14e6d755f8
Chase Almy <chase@chases-air.mynetworksettings.com>
2023-06-24 12:02:56 -0400

    desktop layout almost there

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
     import "../app.css";
 </script>
-  
-<slot />
+<div class="flex justify-center items-center h-screen">
+    <div class="grid sm:grid-cols-[auto,1fr] gap-4 m-auto w-max">
+            <img class=" max-w-[89px] sm:justify-self-end" src="me.png" alt="headshot of me">
+            <div class="self-end">
+                <h1 class="text-3xl font-bold">Chase Almy</h1>
+                <h4>Process Focused UI Developer</h4>
+            </div>
+        
+        <nav class="flex sm:flex-col justify-between sm:justify-self-end">
+            <a href="/about" class=" text-right">About</a>
+            <a href="/experience" class=" text-right">Experience</a>
+            <a href="/projects" class=" text-right">Projects</a>
+        </nav>
+        
+        <slot />
+    </div>
+
+</div>
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<h1 class="mx-80">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<div class="flex">
-    <img src="" alt="">
-    <div class="text-container">
-
-    </div>
-</div>
