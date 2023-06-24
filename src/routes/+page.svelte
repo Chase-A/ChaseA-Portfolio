745f25362e16762f55326a0e8d303682f99c2cdb
Chase Almy <chase@chases-air.mynetworksettings.com>
2023-06-24 10:36:54 -0400

    tailwind setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="mx-80">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="flex">
+    <img src="" alt="">
+    <div class="text-container">
+
+    </div>
+</div>
